0ff19cc86df8ac578c0be55dddd37d47475c43d9
Gustavo Plaza <plazagustavo3@gmail.com>
2026-04-23 19:54:12 -0300

    add generator interface

--- apps/api/services/text/lorem/transport_http.go
+++ apps/api/services/text/lorem/transport_http.go
@@ -7,8 +7,11 @@ import (
 
 	"requiems-api/platform/httpx"
 )
+type Generator interface {
+    Generate(paragraphs, sentences int) Lorem
+}
 
-func RegisterRoutes(r chi.Router, svc *Service) {
+func RegisterRoutes(r chi.Router, svc Generator) {
 	r.Get("/lorem", func(w http.ResponseWriter, r *http.Request) {
 		// Set defaults before binding so unset params keep their default value.
 		req := Request{Paragraphs: 1, Sentences: 5}
