ce45a8156f5cca751d9b6ab65a0cae9dc3f8a4f9
Eliaz Bobadilla <eliaz@bobadilla.tech>
2026-05-06 22:45:37 -0500

    tweaks: thanks god

--- apps/dashboard/config/environments/production.rb
+++ apps/dashboard/config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.public_file_server.enabled = true
   config.action_controller.perform_caching = true
-  config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
+  config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.day.to_i}" }
   config.log_tags = [ :request_id ]
   config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
