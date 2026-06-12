551f9cc79d6c392caeb7399b046f33194670d62e
Kinnouts <kinnouts@gmail.com>
2026-06-12 13:48:29 +0000

    feat: add email validator tool page

---
+++ apps/dashboard/test/controllers/tools_controller_test.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ToolsControllerTest < ActionDispatch::IntegrationTest
+  test "email validator tool page renders successfully" do
+    get "/en/tools/email-validator"
+    assert_response :success
+  end
+
+  test "unknown tool id redirects to root with alert" do
+    get "/en/tools/not-a-real-tool"
+    assert_redirected_to root_path
+    assert_equal "Tool not found.", flash[:alert]
+  end
+end
